798a2363504cab3936e605cb0e177dba38daf95b
wiera <m@m.com>
2024-09-13 10:30:43 +0200

    13.09

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Shape shape = new Shape("Olala");
         System.out.println("shape = " + shape.getValue());
-        Oval oval = new Oval("овальчик")
-        System.out.println("oval.getValue() = " + oval.getValue());
+        Oval oval = new Oval("овальчик");
+        System.out.println("oval=" + oval.getValue());
     }
 }
